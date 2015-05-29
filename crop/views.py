9f8a36731e8b1b689dbb4571625ad064cad73745
matt <mattxbart@gmail.com>
2015-05-28 19:13:24 -0700

    fix downloads

--- crop/views.py
+++ crop/views.py
@@ -1,47 +1,50 @@
 import csv
 from django.http import HttpResponse
 from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import render_to_response
 from crop.models import *
 
 def download_crops(request):
 
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="crops.csv"'
+    response['Content-Disposition'] = 'filename="crops.csv"'
 
     writer = csv.writer(response)
-    writer.writerow(['Crop',
-                     'Type',
-                     'ID',
-                     'Variety', 
-                     'Property', 
-                     'Field Number',
-                     'Acres',
-                     'Plant Date', 
-                     'Harvest Date', 
-                     'Seeds',
-                     'Kernels',
-                     'Moisture',
-                     'Total Yield - tons', 
-                     'Yield - tons per acre',
-                     'Total Yield at 70%',
-                     'Yield - tons per acre at 70%',])
+    rows = []
+    rows.append(['Crop',
+                 'Type',
+                 'ID',
+                 'Variety', 
+                 'Property', 
+                 'Field Number',
+                 'Acres',
+                 'Plant Date', 
+                 'Harvest Date', 
+                 'Moisture',
+                 'Total Yield - tons', 
+                 'Yield - tons per acre',
+                 'Total Yield at 70%',
+                 'Yield - tons per acre at 70%',])
+
     for obj in Crop.objects.all().select_subclasses():
         ct = ContentType.objects.get_for_model(obj)
-        writer.writerow([obj.type.name,
-                         ct.model,
-                         obj.id,
-                         obj.type.variety,
-                         obj.field.name,
-                         obj.field.number,
-                         obj.field.acres,
-                         obj.plant_date,
-                         obj.harvest_date,
-                         obj.moisture,
-                         obj.crop_yield,
-                         obj.yield_per_acre(),
-                         obj.crop_yield_70(),
-                         obj.yield_per_acre_70(),
-                         ])
+        row = [obj.type.name,
+               ct.model,
+               obj.id,
+               obj.type.variety,
+               obj.field.name,
+               obj.field.number,
+               obj.field.acres,
+               obj.plant_date,
+               obj.harvest_date,
+               obj.moisture,
+               obj.crop_yield,
+               str(obj.yield_per_acre()),
+               str(obj.crop_yield_70()),
+               str(obj.yield_per_acre_70()),
+               ]
+        rows.append(row)
+    writer.writerows(rows)
 
     return response
 
@@ -62,8 +65,9 @@ def download_amendments(request):
                      ])
 
     for obj in Amendment.objects.all():
+        
         ct = ContentType.objects.get_for_model(obj.crop)
-        writer.writerow([obj.type.name,
+        writer.writerow([obj.crop.type.name,
                          ct.model,
                          obj.crop.id,
                          obj.date_applied,
